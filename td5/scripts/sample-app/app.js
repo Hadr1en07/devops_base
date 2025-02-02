9726f5adac30bd2a5d5d52870261273f0215d7e5
dejonghh <hadrien.dejonghe@edu.esiee.fr>
2025-02-02 23:10:08 +0100

    Introduce intentional error 2

--- td5/scripts/sample-app/app.js
+++ td5/scripts/sample-app/app.js
@@ -4,8 +4,7 @@ const app = express();
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
-  res.send('\nDevOps Labs!');
+  res.send('DevOps Labs!');
 });
 
 app.get('/name/:name', (req, res) => {
